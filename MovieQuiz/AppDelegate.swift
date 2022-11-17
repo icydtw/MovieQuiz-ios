c8fef7e9f2fabaca88820b97e2f3a39e233074e0
Ilya <a@b-c-d.ru>
2022-11-17 22:37:37 +0500

    Вернул изображение в лонч-скрин.

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -4,6 +4,7 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        sleep(1)
         return true
     }
 
